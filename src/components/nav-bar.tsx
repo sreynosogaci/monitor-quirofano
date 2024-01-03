cf547f2e940d1f961db549b17b38200ef4e05234
@ssreynoso <s.reynosodunjo@gmail.com>
2024-01-03 12:25:17 -0300

    Saco nav-bar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,39 +1,16 @@
 'use client'
 
-import Link from 'next/link'
 // import Image from 'next/image'
 // import Logo from '@/assets/logo.png'
 import { useBoolean, useEventListener } from 'usehooks-ts'
 import { cn } from '@/lib/utils'
-import { Button } from './ui/button'
 // import { useMediaQueryXl } from '@/hooks/use-media-queries'
 import { ModeToggle } from '@/components/dark-mode-toggle-button'
-import { DropdownNavbarMenu } from '@/components/dropdown-navbar-menu'
-import { NavBarOption } from '@/types/utils'
 // import { ChangeTheme } from './change-theme'
-
-const NavOptions = ({ options }: { options: NavBarOption[] }) => (
-    <ul className={cn('hidden xl:flex xl:flex-row xl:gap-5')}>
-        { options.map(op => (
-            <li key={op.value}>
-                <Button variant="link"> 
-                    <Link href={op.value}>{op.label}</Link>
-                </Button>
-            </li>
-        ))}
-    </ul>
-)
 
 
 export const NavBar = () => {
     const { value: moved, setFalse: setMovedFalse, setTrue: setMovedTrue } = useBoolean(false)
-
-    const navOptions = [
-        { label: 'Home', value: '' },
-        { label: 'About', value: '#about' },
-        { label: 'Contact', value: '#contact' },
-        { label: 'Etc...', value: '#etc' },
-    ]
 
     const onScroll = () => {
         if (window.scrollY > 0) {
@@ -55,8 +32,6 @@ export const NavBar = () => {
         >
             <div className="w-full h-full mx-auto max-w-7xl flex justify-center items-center relative">
                 {/* <Image src={Logo} alt='Logo' className='h-2/3 max-h-10 w-max absolute left-0' /> */}
-                <NavOptions options={navOptions}/>
-                <DropdownNavbarMenu options={navOptions} className='xl:hidden'/>
                 <ModeToggle className='absolute right-0 hidden xl:inline-flex'/>
                 {/* <ChangeTheme /> */}
             </div>
